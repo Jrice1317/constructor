a11cd5b0a2ad76f53c49b386813e907aec174143
Jaida Rice <jaidarice@gmail.com>
2025-10-10 08:04:05 -0700

    Update wording of description per review suggestion
    Co-authored-by: Marco Esters <mesters@anaconda.com>

--- constructor/_schema.py
+++ constructor/_schema.py
@@ -836,7 +836,7 @@ class ConstructorConfiguration(BaseModel):
     """
     Protect the base environment with a `frozen` marker file. Requires conda 25.5.0 or newer. This setting can be:
 
-    - `dict`: The `base` environment will be protected via a `frozen` marker file.  If not used, the `base` environment will not be protected. (See CEP-22 for details on the content of the `frozen` marker file.)
+    `dict`: If set, the dictionary will be output into a `frozen` marker file to protect the `base` environment. If not used, the `base` environment will not be protected. See CEP-22 for the specification of the `frozen` file.
     """
 
 
